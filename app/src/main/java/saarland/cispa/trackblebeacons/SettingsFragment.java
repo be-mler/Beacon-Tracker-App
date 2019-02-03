016beca93ccbeedb3d171186726209f43c071ecd
Tobias Faber <github.faberto@itfaber.com>
2019-02-03 15:27:25 +0100

    Feedback button -> Email

--- app/src/main/java/saarland/cispa/trackblebeacons/SettingsFragment.java
+++ app/src/main/java/saarland/cispa/trackblebeacons/SettingsFragment.java
@@ -1,5 +1,7 @@
 package saarland.cispa.trackblebeacons;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.preference.Preference;
@@ -18,6 +20,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         bleTrackerPreferences.LoadSettings(this.getActivity().getApplicationContext());
         SwitchPreferenceCompat switch_sendToCispa = (SwitchPreferenceCompat)findPreference("switch_sendToCispa");
         SwitchPreferenceCompat switch_showBeaconNotifications = (SwitchPreferenceCompat)findPreference("switch_showBeaconNotifications");
+        Preference feedback = findPreference("feedback");
 
         switch_sendToCispa.setOnPreferenceClickListener(preference -> {
             bleTrackerPreferences.setSendToCispa(((SwitchPreferenceCompat)preference).isChecked());
@@ -29,12 +32,27 @@ public class SettingsFragment extends PreferenceFragmentCompat {
             bleTrackerPreferences.SaveSettings(this.getActivity().getApplicationContext());
             return true;
         });
+
+        feedback.setOnPreferenceClickListener(preference -> {
+            composeEmail(new String[]{"ble-app@cispa.saarland"},"BLE Tracker Feedback");
+            return true;
+        });
+
+
         switch_sendToCispa.setChecked(bleTrackerPreferences.isSendToCispa());
         switch_showBeaconNotifications.setChecked(bleTrackerPreferences.isShowBeaconNotifications());
 
 
     }
 
+    public void composeEmail(String[] addresses, String subject) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        startActivity(intent);
+
+    }
 
 
     //TODO: take a look at https://github.com/XinyueZ/preference-demo
